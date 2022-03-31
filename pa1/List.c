3e86aaf82adc2e47a7650798e4daf9ac7a7eba38
Derfel Faller Terciano <dtercian@unix3.lt.ucsc.edu>
2022-03-30 22:52:22 -0700

    updated header

---
+++ pa1/List.c
@@ -0,0 +1,5 @@
+#include "List.h"
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
---
+++ pa1/List.h
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// Constructors-Destructors ---------------------------------------------------
+List newList(void);
+void freeList(List* pL);
+
+// Access functions -----------------------------------------------------------
+
+int length(List l);
+int index(List l);
+int front(List l);
+int back(List l);
+int get(List l);
+bool equals(List A, List B);
+
+// Manipulation procedures ----------------------------------------------------
+
+void clear(List l);
+void set(List l, int x);
+void moveFront(List l);
+void moveBack(List l);
+void movePrev(List l);
+void moveNext(list l);
+void prepend(List l, int x);
+void append(List l, int x);
+void insertBefore(List l, int x);
+void insertAfter(List l, int x);
+
+void deleteFront(List l);
+void deleteBack(List l);
+void delete(List l);
+
+// Other operations -----------------------------------------------------------
+void printList(FILE* out, List l);
+List copyList(List l);
