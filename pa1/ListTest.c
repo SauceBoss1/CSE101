203e331e308beaf9e8af8b76194faef3123950bb
Derfel Faller Terciano <dtercian@unix3.lt.ucsc.edu>
2022-04-03 19:51:23 -0700

    updating

---
+++ pa1/ListTest.c
@@ -0,0 +1,35 @@
+/********************************************************************************* 
+* Derfel Terciano, dtercian 
+* 2022 Spring CSE101 PA1 
+* List.c 
+* Testing harness for List ADT
+*********************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "List.h"
+
+int main(void){
+
+  List A = newList();
+  List B = newList();
+  List C = NULL;
+
+  int i;
+
+  for (i = 0; i <= 50; ++i){
+    append(A, i);
+  }
+  for (i = 0; i <= 50; ++i){
+    prepend(B, i);
+  }
+  printf("-----------------LISTS A & B-----------------\n");
+  printList(stdout, A);
+  printf("\n");
+  printList(stdout, B);
+  printf("----------------------------------------------\n");
+
+  return 0;
+}
