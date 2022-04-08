cf372c17cc4315d80f3e26478b9db146b64a2bd8
dtercian <dtercian@ucsc.edu>
2022-04-08 09:01:58 +0000

    submitting

--- pa2/Graph.c
+++ pa2/Graph.c
@@ -4,3 +4,21 @@
 
 #include "List.h"
 #include "Graph.h"
+
+/*** Structs ***/
+
+// private GraphObj type
+
+typedef struct GraphObj{
+  List* adj_vertices;
+  int* color;
+  int* p;
+  int* distance;
+
+  int vertices;
+  int size;
+  int dource;
+} GraphObj;
+
+/*** Access Functions ***/
+
