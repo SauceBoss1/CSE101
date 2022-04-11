056fa60e0dc3b8546298bf99ed8c458714b50338
dtercian <dtercian@ucsc.edu>
2022-04-11 08:48:11 +0000

    submitting

---
+++ pa2/GraphTest.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "Graph.h"
+#include "List.h"
+
+int main(void){
+   int i, s, max, min, d, n=35;
+   // List  C = newList(); // central vertices 
+   // List  P = newList(); // peripheral vertices 
+   // List  E = newList(); // eccentricities 
+   Graph G = NULL;
+
+   // Build graph G 
+   G = newGraph(n);
+   for(i=1; i<n; i++){
+      if( i%7!=0 ) addEdge(G, i, i+1);
+      if( i<=28  ) addEdge(G, i, i+7);
+   }
+   addEdge(G, 9, 31);
+   addEdge(G, 17, 13);
+   addEdge(G, 14, 33);
+   printGraph(stdout, G);
+   
+   freeGraph(&G);
+
+   return EXIT_SUCCESS;
+}
