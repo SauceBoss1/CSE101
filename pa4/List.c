f1eee752e894d2e3f83f99d6866ed906530e2d1e
dtercian <dtercian@ucsc.edu>
2022-04-24 19:10:23 +0000

    Initial set-up (PUSHING WITH DERFEL's GIT SCRIPT)

---
+++ pa4/List.c
@@ -0,0 +1,562 @@
+/********************************************************************************* 
+* Derfel Terciano, dtercian 
+* 2022 Spring CSE101 PA1 
+* List.c 
+* Implementation for the List ADT 
+*********************************************************************************/
+
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#include "List.h"
+
+// structs ----------------------------------------------------
+
+//private Node type
+typedef struct NodeObj* Node;
+
+// private NodeObj type
+typedef struct NodeObj {
+
+  ListElement data;
+  Node next;
+  Node prev;
+
+} NodeObj;
+
+// private ListObj type
+typedef struct ListObj{
+  Node front, cursor, back;
+  int index;
+  int length;
+} ListObj;
+
+// Constructors-Deconstructors --------------------------------
+
+/*
+newNode(int data)
+creates a new Node and returns the new node
+*/
+Node newNode(ListElement data){
+  Node N = malloc(sizeof(NodeObj));
+  N->data = data;
+  N->next = NULL;
+  N->prev = NULL;
+  return(N);
+}
+
+/*
+freeNode(Node* pN)
+"deletes" and free a given node
+*/
+void freeNode(Node* pN){
+  if( pN != NULL && *pN != NULL){
+    free(*pN);
+    *pN=NULL;
+  }
+}
+
+/*
+newList()
+Creates a new list and returns it
+*/
+List newList(void){
+  List L = malloc(sizeof(ListObj));
+  L->cursor = L->front = L->back = NULL;
+  L->length = 0;
+  L->index = -1;
+  return L;
+}
+
+/*
+freeList(List* pL)
+"Deletes" and frees a list
+
+The implementation is similar to that of the Queue example
+given to us by prof. Tantalo
+*/
+void freeList(List* pL){
+  if(pL != NULL && *pL!=NULL){
+    while( length(*pL) > 0){
+      deleteBack(*pL);
+    }
+    free(*pL);
+    *pL = NULL;
+  }
+}
+
+// Access functions -----------------------------------------------------------
+
+/*
+length(List l)
+Returns the length of a list
+
+Pre: l == NULL
+*/
+int length(List l){
+  if(l == NULL){
+    printf("ERROR: Checking the length of a NULL list.\n");
+    exit(EXIT_FAILURE);
+  }
+  return l->length;
+}
+
+/*
+index(List l)
+Returns the index of the list
+
+index = -1 if undefined
+Pre: l == NULL
+*/
+int index(List l){
+  if(l == NULL){
+    printf("ERROR: Checking the index of a NULL list.\n");
+    exit(EXIT_FAILURE);
+  }
+  return l->index;
+}
+
+/*
+front(list l)
+Returns the front element
+
+Pre: l == NULL & length < 0
+*/
+ListElement front(List l){
+  if(l == NULL){
+    printf("ERROR: Checking the front element of a NULL list.\n");
+    exit(EXIT_FAILURE);
+  }
+  if(length(l) < 0){
+    printf("ERROR: The length of the list is 0\n");
+    exit(EXIT_FAILURE);
+  }
+  return l->front->data;
+}
+
+/*
+back(List l)
+Returns the back element
+
+Pre: l == NULL & length < 0
+*/
+ListElement back(List l){
+  if(l == NULL){
+    printf("ERROR: Checking the back element of a NULL list.\n");
+    exit(EXIT_FAILURE);
+  }
+  if(length(l) < 0){
+    printf("ERROR: The length of the list is 0\n");
+    exit(EXIT_FAILURE);
+  }
+  return l->back->data;
+}
+
+/*
+get(List l)
+Returns the element that is currently at the cursor
+
+Pre: l == NULL & length < 0
+*/
+ListElement get(List l){
+  if(l == NULL){
+    printf("ERROR: Getting the element of a NULL list.\n");
+    exit(EXIT_FAILURE);
+  }
+  if(length(l) < 0){
+    printf("ERROR: The length of the list is 0\n");
+    exit(EXIT_FAILURE);
+  }
+  return l->cursor->data;
+}
+
+
+// Manipulation procedures ----------------------------------------------------
+
+/*
+clear(List l)
+Resets the list to its empty state
+
+Pre: l == NULL
+*/
+void clear(List l){
+  if(l == NULL){
+    printf("ERROR: Clearing a NULL list.\n");
+    exit(EXIT_FAILURE);
+  }
+  
+  while( length(l) > 0){
+    deleteBack(l);
+  }
+  l->front = l->cursor = l->back = NULL;
+  l->index = -1;
+  l->length = 0;
+  return;
+}
+
+/*
+set(List l, int x)
+Replaces the cursors data with x
+
+Pre: l == NULL & length < 0
+*/
+void set(List l, ListElement x){
+  if(l == NULL){
+    printf("ERROR: Setting an element of a NULL list\n");
+    exit(EXIT_FAILURE);
+  }
+  if (length(l) < 0 ){ 
+    printf("ERROR: Setting an element to an empty list.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if(index(l) != -1){
+    l->cursor->data = x;
+  }
+  return;
+}
+
+/*
+moveFront(List l)
+Moves the cursor to the front
+
+Pre: l == NULL
+*/
+void moveFront(List l){
+  if(l == NULL){
+    printf("ERROR: Trying to move cursor to the front of a NULL list\n");
+    exit(EXIT_FAILURE);
+  }
+  if (length(l) > 0){
+    l->cursor = l->front;
+    l->index = 0;
+  }
+
+  return;
+}
+
+/*
+moveBack(List l)
+Moves the cursor to the front
+
+Pre: l == NULL
+*/
+void moveBack(List l){
+  if(l == NULL){
+    printf("ERROR: Trying to move cursor to the back of a NULL list\n");
+    exit(EXIT_FAILURE);
+  }
+  if (length(l) > 0){
+    l->cursor = l->back;
+    l->index = length(l) - 1;
+  }
+}
+
+/*
+movePrev(List l)
+Moves the cursor to the previous element
+
+Pre: l == NULL
+*/
+void movePrev(List l){
+  if(l == NULL){
+    printf("ERROR: Trying to move cursor one step to the front of a NULL list\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if(l->cursor != NULL && l->cursor->prev == NULL){
+    l->cursor = NULL;
+    l->index = -1;
+  } else if(l->cursor != NULL && l->cursor->prev != NULL){
+    l->cursor = l->cursor->prev;
+    l->index--;
+  } 
+  return;
+}
+
+/*
+moveNext(List l)
+Moves the cursor to the next element
+
+Pre: l == NULL
+*/
+void moveNext(List l){
+  if(l == NULL){
+    printf("ERROR: Trying to move cursor one step to the back of a NULL list\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if(l->cursor != NULL && l->cursor->next == NULL){
+    l->cursor = NULL;
+    l->index = -1;
+  } else if(l->cursor->next != NULL && l->cursor != NULL){
+    l->cursor = l->cursor->next;
+    l->index++;
+  }
+  return;
+}
+
+/*
+prepend(List l, int x)
+Adds a new element to the beginning of the list
+
+Pre: l == NULL
+*/
+void prepend(List l, ListElement x){
+  if(l==NULL){
+    printf("ERROR: Trying to prepend to a NULL list\n");
+    exit(EXIT_FAILURE);
+  }
+  
+  Node n = newNode(x);
+  if(l->front == NULL && l->back==NULL && length(l) == 0){ //if we are adding to an empty list
+    l->front = n;
+    l->back = n;
+    l->length++;
+  } else {
+    l->front->prev = n;
+    n->next = l->front;
+    l->front = n;
+    l->length++;
+  }
+  if(index(l) >= 0 ){
+    l->index++;
+  }
+  return;
+}
+
+/*
+append(List l, int x)
+Adds a new element to the end of the list
+
+Pre: l == NULL
+*/
+void append(List l, ListElement x){
+  if(l==NULL){
+    printf("ERROR: Trying to append to a NULL list\n");
+    exit(EXIT_FAILURE);
+  }
+
+  Node n = newNode(x);
+  if(l->front == NULL && l->back == NULL && length(l) == 0){ //if the list is empty
+    l->front = l->back = n;
+    l->length++;
+  } else {
+    l->back->next = n;
+    n->prev = l->back;
+    l->back = n;
+    l->length++;
+  }
+  return;
+}
+
+/*
+insertBefore(List l, int x)
+Inserts a new element before the cursor
+
+Pre: l == NULL & length < 0 & index == -1
+*/
+void insertBefore(List l, ListElement x){
+  if(l==NULL){
+    printf("ERROR: Trying to insert before cursor on a NULL list\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if(length(l) < 0){
+    printf("ERROR: Inserting to an empty list\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if(index(l) == -1){
+    printf("ERROR: Inserting to an undefined cursor\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if(l->cursor->prev == NULL && length(l) > 0 && index(l) >=0){
+    prepend(l,x);
+  } else if(length(l) > 0 && index(l) >=0){
+    Node n = newNode(x);
+
+    n->next = l->cursor;
+    n->prev = l->cursor->prev;
+    l->cursor->prev = n;
+    n->prev->next = n;
+    l->index++;
+    l->length++;
+  }
+  return;
+}
+
+/*
+insertAfter(List l, int x)
+Inserts a new element after the cursor
+
+Pre: l == NULL & length < 0 & index == -1
+*/
+void insertAfter(List l, ListElement x){
+  if(l==NULL){
+    printf("ERROR: Trying to insert after cursor on a NULL list\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if(length(l) < 0){
+    printf("ERROR: Inserting to an empty list\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if(index(l) == -1){
+    printf("ERROR: Inserting to an undefined cursor\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if(l->cursor->next == NULL && length(l) > 0 && index(l) >= 0){
+    append(l,x);
+  } else if(length(l) > 0 && index(l) >=0){
+    Node n = newNode(x);
+
+    n->prev = l->cursor;
+    n->next = l->cursor->next;
+    l->cursor->next = n;
+    n->next->prev = n;
+    l->length++;
+  }
+  return;
+}
+
+/*
+deleteFront(List l)
+deletes the front element
+
+Pre: l==NULL & length < 0
+*/
+void deleteFront(List l){
+  if(l==NULL){
+    printf("ERROR: Trying to delete front on a NULL list\n");
+    exit(EXIT_FAILURE);
+  }
+  if(length(l) < 0){
+    printf("ERROR: Deleting the front element of an empty list\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if(index(l) == 0){
+    l->cursor = NULL;
+    l->index = -1;
+  }
+
+  if(length(l) > 0 && l->front->next == NULL){
+    freeNode(&(l->front));
+    l->front = NULL;
+    l->back = NULL;
+    l->cursor = NULL;
+    l->index = -1;
+    l->length--;
+    return;
+  }
+
+  if(length(l) > 0){
+    Node tempNode = l->front;
+    l->front = l->front->next;
+    l->front->prev = NULL;
+    freeNode(&tempNode);
+    l->length--;
+    if(index(l) != -1){
+      l->index--;
+    }
+  }
+  return;
+}
+
+/*
+deleteBack(List l)
+deletes the back element
+
+Pre: l==NULL & length < 0
+*/
+void deleteBack(List l){
+  if(l==NULL){
+    printf("ERROR: Trying to delete back on a NULL list\n");
+    exit(EXIT_FAILURE);
+  }
+  if(length(l) < 0){
+    printf("ERROR: Deleting the back element of an empty list\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if(index(l) == length(l) - 1){
+    l->cursor = NULL;
+    l->index = -1;
+  }
+
+  if(length(l) > 0 && l->back->prev == NULL){
+    freeNode(&(l->back));
+    l->front = NULL;
+    l->back = NULL;
+    l->cursor = NULL;
+    l->index = -1;
+    l->length--;
+    return;
+  }
+
+  if(length(l) > 0){
+    Node tempNode = l->back;
+    l->back = l->back->prev;
+    l->back->next = NULL;
+    freeNode(&tempNode);
+    l->length--;
+  }
+  return;
+}
+
+/*
+delete(List l)
+Deletes the element at the cursor
+
+Pre: l==NULL & length < 0 & index < 0
+*/
+void delete(List l){
+  if(l==NULL){
+    printf("ERROR: Trying to delete cursor element on a NULL list\n");
+    exit(EXIT_FAILURE);
+  }
+  if(length(l) < 0){
+    printf("ERROR: Deleting the back element of an empty list\n");
+    exit(EXIT_FAILURE);
+  }
+  
+  if(l->cursor == NULL){
+    fprintf(stderr, "ERROR: Can't delete a null cursor\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if(l->cursor->next == NULL && l->cursor->prev == NULL && length(l) > 0 && index(l) >= 0){
+    freeNode(&(l->cursor));
+    l->front = NULL;
+    l->back = NULL;
+    l->cursor = NULL;
+    l->index = -1;
+    l->length--;
+  } else if(l->cursor->next == NULL && length(l) > 0 && index(l) >= 0){
+    deleteBack(l);
+    l->cursor = NULL;
+    l->index = -1;
+  } else if (l->cursor->prev == NULL && length(l) > 0 && index(l) >= 0){
+    deleteFront(l);
+    l->cursor = NULL;
+    l->index = -1;
+  } else if(length(l) > 0 && l->cursor != NULL){
+    Node temp = l->cursor;
+    l->cursor->next->prev = l->cursor->prev;
+    l->cursor->prev->next = l->cursor->next;
+    freeNode(&temp);
+    l->cursor = NULL;
+    l->index = -1;
+    l->length--;
+  }
+
+  return;
+}
+
+// Other operations -----------------------------------------------------------
---
+++ pa4/List.h
@@ -0,0 +1,49 @@
+/********************************************************************************* 
+* Derfel Terciano, dtercian 
+* 2022 Spring CSE101 PA1 
+* List.h 
+* Header file for the List ADT 
+*********************************************************************************/
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#pragma once
+
+// Exported Types -------------------------------------------------------------
+
+typedef struct ListObj* List;
+typedef void* ListElement;
+
+// Constructors-Destructors ---------------------------------------------------
+
+List newList(void);
+void freeList(List* pL);
+
+// Access functions -----------------------------------------------------------
+
+int length(List l);
+int index(List l);
+ListElement front(List l);
+ListElement back(List l);
+ListElement get(List l);
+
+// Manipulation procedures ----------------------------------------------------
+
+void clear(List l);
+void set(List l, ListElement x);
+void moveFront(List l);
+void moveBack(List l);
+void movePrev(List l);
+void moveNext(List l);
+void prepend(List l, ListElement x);
+void append(List l, ListElement x);
+void insertBefore(List l, ListElement x);
+void insertAfter(List l, ListElement x);
+
+void deleteFront(List l);
+void deleteBack(List l);
+void delete(List l);
+
+// Other operations -----------------------------------------------------------
