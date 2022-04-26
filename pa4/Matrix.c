cd1e444ef37f2d5cc2341238c51df09ed85e79fb
dtercian <dtercian@ucsc.edu>
2022-04-26 17:52:15 +0000

    Updating Matrix.c (PUSHING WITH DERFEL's GIT SCRIPT)

--- pa4/Matrix.c
+++ pa4/Matrix.c
@@ -83,3 +83,50 @@ int size(Matrix M){
    }
    return M->size;
 }
+
+int NNZ(Matrix M){
+   if(M ==  NULL){
+      printf("MATRIX ERROR: Calling NNS() on a NULL Matrix\n");
+      exit(EXIT_FAILURE);
+   }
+   return M->NNZ;
+}
+
+/**
+ * Private helper fucntion
+ * bool vectorEquals(List P, List Q)
+ * Checks whether or two vectors are equal or not
+ */
+bool vectorEquals(List P, List Q){
+   if(P == NULL || Q == NULL){
+      printf("vectorEquals ERROR: List P or Q is NULL!\n");
+      exit(EXIT_FAILURE);
+   }
+   if( length(P) != length(Q)){
+      return false;
+   }
+
+   for(moveFront(P), moveFront(Q); index(P) >= 0 && index(Q) >=0; moveNext(P), moveNext(Q)){
+      Entry ent_p = *(Entry*)get(P);
+      Entry ent_q = *(Entry*)get(Q);
+      if(!(ent_p->col == ent_q->col && ent_p->val == ent_q->val)){
+         return false;
+      }
+      return true;
+   }
+   return true;
+}
+
+int equals(Matrix A, Matrix B){
+   if(size(A) != size(B)){
+      return 0;
+   }
+
+   for(int i = 1; i < size(A) + 1; ++i){
+      if(!(vectorEquals(A->m_body[i], B->m_body[i]))){
+         return 0;
+      }
+   }
+   return 1;
+}
+
