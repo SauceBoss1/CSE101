68f33c21ab0e52796dd4ffec7f89c929ce525c6e
dtercian <dtercian@ucsc.edu>
2022-04-27 07:48:34 +0000

    update (PUSHING WITH DERFEL's GIT SCRIPT)

--- pa4/Matrix.h
+++ pa4/Matrix.h
@@ -32,7 +32,8 @@ int NNZ(Matrix M);
 // Return true (1) if matrices A and B are equal, false (0) otherwise.
 int equals(Matrix A, Matrix B);
 
-// Manipulation procedures
+/*** MANIPULATION PROCEDURES ***/
+
 // makeZero()
 // Re-sets M to the zero Matrix state.
 void makeZero(Matrix M);
@@ -40,6 +41,8 @@ void makeZero(Matrix M);
 // Changes the ith row, jth column of M to the value x.
 // Pre: 1<=i<=size(M), 1<=j<=size(M)
 void changeEntry(Matrix M, int i, int j, double x);
+
+/*** DEBUG FUNCTIONS ***/
 
 // printMatrix()
 // Prints a string representation of Matrix M to filestream out. Zero rows
