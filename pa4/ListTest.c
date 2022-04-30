47c2dafd33ea93a9284a6bab9bdc40d365ff359f
dtercian <dtercian@ucsc.edu>
2022-04-30 21:00:23 +0000

    Created and updating ListTest (PUSHING WITH DERFEL's GIT SCRIPT)

---
+++ pa4/ListTest.c
@@ -0,0 +1,51 @@
+/********************************************************************************* 
+* Derfel Terciano, dtercian 
+* 2022 Spring CSE101 PA4
+* ListTest.c 
+* Main test harness for the modified List ADT
+*********************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "List.h"
+
+/**
+ * printList()
+ * Custom printList function for test harness
+ */
+void printList(List L){
+   for(moveFront(L); index(L) >=0; moveNext(L)){
+      printf("%d ", *(int *)get(L));
+   }
+   printf("\n");
+   return;
+}
+
+int main(void){
+   List A,B;
+   A = newList(); B = newList();
+
+   int X[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+
+   for(int i = 1; i < 21; ++i){
+      append(A,&X[i]);
+      prepend(B,&X[i]);
+   }
+   printList(A);
+   printList(B);
+   printf("Length of A: %d\n", length(A));
+   printf("Length of B: %d\n", length(B));
+
+   for(moveFront(A); index(A) <= 10; moveNext(A)){
+      printf("index(A) = %d, element: %d\n", index(A), *(int *)get(A));
+   }
+
+   printf("Front of B: %d\n", *(int *)front(B));
+   printf("Front of B: %d\n", *(int *)back(B));
+   
+
+   freeList(&A);
+   freeList(&B);
+   return EXIT_SUCCESS;
+}
