7382f35ec18bff91bcf7a8b502141042d2650d85
dtercian <dtercian@ucsc.edu>
2022-05-04 08:21:21 +0000

    Added Access funcs (PUSHING WITH DERFEL'S PUSH SCRIPT)

--- pa5/List.cpp
+++ pa5/List.cpp
@@ -27,3 +27,63 @@ List::List(){
    int pos_cursor = 0;
    int num_elements = 0;
 }
+
+List::List(const List& L){
+   frontDummy = new Node(FRONT_NIL);
+   backDummy = new Node(BACK_NIL);
+   beforeCursor = frontDummy;
+   afterCursor = backDummy;
+
+   for(Node* N = L.frontDummy->next; N != nullptr; N = N->next ){
+      this->setBefore(N->data);
+   }
+}
+
+List::~List(){
+   while( num_elements > 0){
+      eraseBefore();
+   }
+   beforeCursor = nullptr;
+   afterCursor = nullptr;
+   delete frontDummy;
+   delete backDummy;
+}
+
+/*** ACCESS FUNCTIONS ***/
+
+int List::length() const{
+   return num_elements;
+}
+
+ListElement List::front() const{
+   if (num_elements < 0){
+      throw std::length_error("List: front(): empty List\n");
+   }
+   return frontDummy->next->data;
+}
+
+ListElement List::back() const{
+   if(num_elements < 0){
+      throw std::length_error("List: back(): empty List\n");
+   }
+   return backDummy->prev->data;
+}
+
+int List::position() const{
+   return pos_cursor;
+}
+
+ListElement List::peekNext() const{
+   if(position() > length()){
+      throw std::length_error("List: peekNext(): position > length\n");
+   }
+   return afterCursor->data;
+}
+
+ListElement List::peekPrev() const{
+   if(position() > length()){
+      throw std::length_error("List: peekPrev(): position > length\n");
+   }
+   return beforeCursor->data;
+}
+
