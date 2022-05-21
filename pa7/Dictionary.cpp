0e974ef244764ccf46d9bc11222057c2cf6fc99c
dtercian <dtercian@ucsc.edu>
2022-05-21 02:42:08 +0000

    initial cpp (PUSHING WITH DERFEL'S GIT SCRIPT)

--- pa7/Dictionary.cpp
+++ pa7/Dictionary.cpp
@@ -3,3 +3,24 @@
 
 #include "Dictionary.h"
 
+#define NIL_VAL 2147483647
+
+/*** PRIVATE CONSTRUCTORS ***/
+
+//Node Constructor
+Dictionary::Node::Node(keyType k, valType v){
+   key = k;
+   val = v;
+   parent = nullptr;
+   left = nullptr;
+   right = nullptr;
+}
+
+/*** CLASS CONSTRUCTORS AND DESTRUCTORS ***/
+
+Dictionary::Dictionary(){
+   nil = new Node("",NIL_VAL);
+   root = nullptr;
+   current = nullptr;
+   num_pairs = 0;
+}
